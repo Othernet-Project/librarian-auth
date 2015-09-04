aa1fba96abaf52724df9c05d2b2a2a57bc966f56
Andrean Franc <andrean.franc@gmail.com>
2015-09-04 10:50:43 +0200

    Fix packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-import librarian_auth
+import librarian_auth as pkg
 
 
 def read(fname):
@@ -9,13 +9,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-VERSION = librarian_auth.__version__
+VERSION = pkg.__version__
 
 setup(
     name='librarian-auth',
     version=VERSION,
     license='BSD',
-    packages=find_packages(),
+    packages=[pkg.__name__],
+    include_package_data=True,
     long_description=read('README.rst'),
     install_requires=[
         'pbkdf2',
