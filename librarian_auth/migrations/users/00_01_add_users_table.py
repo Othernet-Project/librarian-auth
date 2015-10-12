79e7a24649e349fa541673e4caf3f301d89a1829
Andrean Franc <andrean.franc@gmail.com>
2015-10-12 14:58:20 +0200

    Delete obsolete is_superuser column

--- librarian_auth/migrations/users/00_01_add_users_table.py
+++ librarian_auth/migrations/users/00_01_add_users_table.py
@@ -3,7 +3,6 @@ create table users
 (
     username varchar primary_key unique not null,   -- username
     password varchar not null,                      -- encrypted password
-    is_superuser boolean not null default 0,        -- is admin user
     created timestamp not null,                     -- user creation timestamp
     options varchar default '{}',                   -- arbitary user options in json
     reset_token text,                               -- password reset token
