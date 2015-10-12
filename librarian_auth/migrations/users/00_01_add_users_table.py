d50126e7629d705b8aa0a07a8b7d150451fa96c6
Andrean Franc <andrean.franc@gmail.com>
2015-10-12 13:59:01 +0200

    Squash migrations, dropping sessions and notifications tables out

--- librarian_auth/migrations/users/00_01_add_users_table.py
+++ librarian_auth/migrations/users/00_01_add_users_table.py
@@ -4,7 +4,10 @@ create table users
     username varchar primary_key unique not null,   -- username
     password varchar not null,                      -- encrypted password
     is_superuser boolean not null default 0,        -- is admin user
-    created timestamp not null                      -- user creation timestamp
+    created timestamp not null,                     -- user creation timestamp
+    options varchar default '{}',                   -- arbitary user options in json
+    reset_token text,                               -- password reset token
+    groups text                                     -- comma separated list of groups
 );
 """
 
