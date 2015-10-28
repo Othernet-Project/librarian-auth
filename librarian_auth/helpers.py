25c2d36bf14f96d368913ba284ecb68a93471151
Andrean Franc <andrean.franc@gmail.com>
2015-10-28 15:19:01 +0100

    Use new helper for setting option

--- librarian_auth/helpers.py
+++ librarian_auth/helpers.py
@@ -1,4 +1,6 @@
-from bottle import request, response
+from bottle import request
+
+from librarian_core.contrib.i18n.utils import set_current_locale
 
 from .options import Options
 
@@ -11,8 +13,7 @@ def collect_language(options):
 @Options.processor('language', is_explicit=True)
 def process_language(options, language):
     if language and request.locale != language:
-        request.locale = language
-        response.set_cookie('locale', language, path='/')
+        set_current_locale(language)
 
 
 @Options.processor('default_route')
